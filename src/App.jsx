1262613624bd5c4fd26b1e209061db2c2e4c41ad
ranaintizar <intizaralirana2@gmail.com>
2022-12-03 13:39:52 +0500

    Minor Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
-import Header from "./components/header/header";
-import Footer from "./components/footer/footer";
+// import Header from "./components/header/header";
+// import Footer from "./components/footer/footer";
 
 const App = () => {
   return (
