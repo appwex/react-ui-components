28c6aa93f4120bb496fdd11582da6215cc59bc20
ranaintizar <intizaralirana2@gmail.com>
2022-12-03 13:38:27 +0500

    Minor Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
 
 const App = () => {
   return (
     <div className="app">
+      {/* <header>
+        <Header />
+      </header> */}
       <main className="main">
         <Outlet />
       </main>
+      {/* <footer>
+        <Footer />
+      </footer> */}
     </div>
   );
 };
