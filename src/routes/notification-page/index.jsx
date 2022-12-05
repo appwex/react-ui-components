6b2d10ce704bc092e625d61bb32887342a085da4
mrxshinji <95459307+mrxshinji@users.noreply.github.com>
2022-12-05 17:32:10 +0800

    add notification bar component

---
+++ src/routes/notification-page/index.jsx
@@ -0,0 +1,11 @@
+import Notifications from '../../components/notifications/notifications';
+
+const NotificationPage = () => {
+  return (
+    <div>
+      <Notifications />
+    </div>
+  );
+};
+
+export default NotificationPage;
