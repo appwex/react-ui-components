d4ad9b949582c1a51fa5dc392bafc0af5d5c9030
tehseen <codebyishan@gmail.com>
2022-12-05 20:49:56 +0530

    first commit (#25)

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import Homepage from './routes/homepage/homepage';
 import TestPage from './routes/test-page.jsx/test-page';
 import ComponentStory from './routes/story-page/componet';
 import ButtonStory from './routes/story-page/button';
+import QuickTransfer from './components/quickTransfer/QuickTransfer';
 
 const router = createBrowserRouter([
   {
@@ -37,6 +38,10 @@ const router = createBrowserRouter([
       {
         path: 'stories/button',
         element: <ButtonStory />,
+      },
+      {
+        path: '/quicktransfer',
+        element: <QuickTransfer />,
       },
     ],
   },
