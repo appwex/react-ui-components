f0ac4ef343cce632ce517d9d12d08cc9e156b7b9
mrxshinji <95459307+mrxshinji@users.noreply.github.com>
2022-12-06 13:15:03 +0800

    Add Notification-Bar Component  (#26)
    * add notification bar component
    
    * Update src/components/notifications/notificationBar.css
    
    Co-authored-by: Aftab Rehan <workwithaftabrehan@gmail.com>
    
    * Update src/components/notifications/notificationBar.css
    
    Co-authored-by: Aftab Rehan <workwithaftabrehan@gmail.com>
    
    * Update src/components/notifications/notificationBar.css
    
    Co-authored-by: Aftab Rehan <workwithaftabrehan@gmail.com>
    
    * Update src/components/notifications/notificationBar.css
    
    Co-authored-by: Aftab Rehan <workwithaftabrehan@gmail.com>
    
    * Update src/components/notifications/notificationBar.css
    
    Co-authored-by: Aftab Rehan <workwithaftabrehan@gmail.com>
    
    * Update src/components/notifications/notifications.css
    
    Co-authored-by: Aftab Rehan <workwithaftabrehan@gmail.com>
    
    * Update src/components/notifications/notifications.css
    
    Co-authored-by: Aftab Rehan <workwithaftabrehan@gmail.com>
    
    * Update src/index.js
    
    Co-authored-by: Aftab Rehan <workwithaftabrehan@gmail.com>
    
    * Update src/routes/notification-page/index.jsx
    
    Co-authored-by: Aftab Rehan <workwithaftabrehan@gmail.com>
    
    * Update src/routes/notification-page/index.jsx
    
    Co-authored-by: Aftab Rehan <workwithaftabrehan@gmail.com>
    
    * added hover effect and changes as requested
    
    * added link to component
    
    * Fix prettier and adjust css line spacing scheme
    
    * Minor adjustment
    
    Co-authored-by: Aftab Rehan <workwithaftabrehan@gmail.com>
    Co-authored-by: aftabrehan <aftabrehan52422@gmail.com>

--- src/components/notifications/notificationBar.jsx
+++ src/components/notifications/notificationBar.jsx
@@ -1,11 +1,75 @@
-const NotificationBar = () => {
+import './notificationBar.css';
+
+import { ReactComponent as SendIcon } from '../../assets/svg/send.svg';
+import { ReactComponent as ReceiveIcon } from '../../assets/svg/receive.svg';
+import { ReactComponent as NotificationErrorIcon } from '../../assets/svg/notification-error.svg';
+
+const NotificationBar = ({
+  idx,
+  type,
+  to = '',
+  from = '',
+  amount = 0,
+  unit = '',
+  isActive = false,
+  handleIsActiveBar,
+}) => {
   return (
-    <div className="alert-bar">
-      <div className="alert-icon">icon</div>
-      <div className="alert-text">
-        <p>Your money has been delivered</p>
-        <p>Awesome, Karios has received your money</p>
-      </div>
+    <div
+      className={`notification-bar-container ${isActive && 'active'}`}
+      value={idx}
+      onClick={() => handleIsActiveBar(idx)}
+    >
+      {type === 'send' && (
+        <>
+          <span className={`notification-icon ${isActive && 'active'}`}>
+            <SendIcon />
+          </span>
+          <div className="notification-text">
+            <p className={`text-title ${isActive && 'active'}`}>
+              Your money has been delivered
+            </p>
+            <p className="text-subtitle">
+              Awesome, {to} has received your money
+            </p>
+          </div>
+        </>
+      )}
+
+      {type === 'error' && (
+        <>
+          <span className={`notification-icon ${isActive && 'active'}`}>
+            <NotificationErrorIcon />
+          </span>
+          <div className="notification-text">
+            <p className={`text-title ${isActive && 'active'}`}>
+              Etehreum wallet watch alert!
+            </p>
+            <p className="text-subtitle">
+              Your Ethereum wallet has been unusual...
+            </p>
+          </div>
+        </>
+      )}
+
+      {type === 'receive' && (
+        <>
+          <span className={`notification-icon ${isActive && 'active'}`}>
+            <ReceiveIcon />
+          </span>
+          <div className="notification-text">
+            <p className={`text-title ${isActive && 'active'}`}>
+              You just received
+            </p>
+            <p className="text-subtitle">
+              {from} just sent you{' '}
+              <strong>
+                {amount} {unit}.{' '}
+              </strong>
+            </p>
+          </div>
+        </>
+      )}
     </div>
   );
 };
