75dc4ba245e95df1644774dbd57fc6f44660d6e2
abdulmaleekadams <83005894+abdulmaleekadams@users.noreply.github.com>
2022-12-08 07:33:22 -0800

    ADD NAVBAR COMPONENT (#31) (#40)
    * Add Navbar Componenet (#31)
    
    * Move all svg into a folder, Remove the React-icon Library and replace with svg
    
    * import cookie from route in src/index.js
    
    * modified index.js to fix conflict
    
    Co-authored-by: mrxshinji <95459307+mrxshinji@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
-// import Header from "./components/header/header";
+import Header from './components/header/header'
 // import Footer from "./components/footer/footer";
 
 const App = () => {
   return (
     <div className="app">
-      {/* <header>
+      <header>
         <Header />
-      </header> */}
+      </header>
       <main className="main">
         <Outlet />
       </main>
