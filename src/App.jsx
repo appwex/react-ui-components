14699d2387bfcc0cfa2e689f5260fc8b7ed0be1e
Whalay <soliuy5@gmail.com>
2022-12-14 10:35:49 +0100

    made some changes on the component page

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import { Outlet } from 'react-router-dom'
 import Header from './components/header/header'
 // import Footer from "./components/footer/footer";
 
-import HeaderSearchBar from 'components/headerWithSearchBar/HeaderSearchBar'
-
 const App = () => {
   return (
     <div className="app">
@@ -13,7 +11,6 @@ const App = () => {
       </header>
       <main className="main">
         <Outlet />
-        <HeaderSearchBar />
       </main>
       {/* <footer>
         <Footer />
