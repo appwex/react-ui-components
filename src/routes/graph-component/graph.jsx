0d49435c049b0443415286d9769dc4e29bf5926b
Sarosh Farhan <saroshfarhan6495@gmail.com>
2022-12-14 11:53:49 +0530

    installed recharts

--- src/routes/graph-component/graph.jsx
+++ src/routes/graph-component/graph.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 import './graph.css'
 
 function Graph() {
+  const [value, setValue] = React.useState('Quarter')
+
+  const handleChange = event => {
+    setValue(event.target.value)
+  }
   return (
     <div className="main-container">
-      <h1>Graph component here</h1>
+      <div className="graph-container">
+        <div className="header-data">
+          <div className="main-data">
+            <h1>$58,092.10</h1>
+            <p>
+              <span>+530.21%</span> over the past quarter
+            </p>
+          </div>
+          <div className="dropdown">
+            <select value={value} onChange={handleChange}>
+              <option value="quarter">Quarter</option>
+              <option value="year">Year</option>
+            </select>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
