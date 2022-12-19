60ffaeed70319d3b984474104c810edee66abc96
mrxshinji <95459307+mrxshinji@users.noreply.github.com>
2022-12-19 16:01:13 +0800

    page conflict-fix

--- src/routes/story-page/story-page.jsx
+++ src/routes/story-page/story-page.jsx
@@ -7,7 +7,6 @@ const StoryPage = () => {
       <h1>Stories of components</h1>
       <Link to={`/`}>Back to home</Link>
       <Link to={`/stories/component`}>A-component</Link>
-      <Link to={`/stories/button`}>Example Button</Link>
       <Link to={`/stories/quicktransfer`}>Quick transfer</Link>
       <Link to={`/stories/notification`}>Notification Bar</Link>
       <Link to={`/stories/payment`}> Payment </Link>
